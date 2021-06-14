4c22c749d9a4a893bf5355fed1c97a22ca73b3dd
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-06-14 21:13:38 +0530

    fix: add more companies

--- assets/companies.ts
+++ assets/companies.ts
@@ -1,39 +1,91 @@
+import _314eCorp from '@assets/314e.svg';
+import Accops from '@assets/accops.svg';
 import Adobe from '@assets/adobe.svg';
 import AlcatelLucent from '@assets/alcatellucent.svg';
 import Amazon from '@assets/amazon.svg';
+import BrowserStack from '@assets/browserstack.svg';
+import Byjus from '@assets/byjus.svg';
 import Capgemini from '@assets/capgemini.svg';
 import CGI from '@assets/cgi.svg';
+import Cvent from '@assets/cvent.svg';
+import Delhivery from '@assets/delhivery.svg';
 import Deloitte from '@assets/deloitte.svg';
 import Ericsson from '@assets/ericsson.svg';
+import Fampay from '@assets/fampay.svg';
 import Fanatics from '@assets/fanatics.svg';
+import FiftyFive from '@assets/fiftyfive.svg';
 import Flipkart from '@assets/flipkart.svg';
 import GoldmanSachs from '@assets/goldmansachs.svg';
+import Icertis from '@assets/icertis.svg';
 import Infosys from '@assets/infosys.svg';
-import JIO from '@assets/jio.svg';
+import Invenio from '@assets/invenio.svg';
+import Jio from '@assets/jio.svg';
 import Microsoft from '@assets/microsoft.svg';
+import Nagarro from '@assets/nagarro.svg';
+import NeoSoft from '@assets/neosoft.svg';
+import Newgen from '@assets/newgen.svg';
 import Oracle from '@assets/oracle.svg';
+import Orange from '@assets/orange.svg';
+import OYO from '@assets/oyo.svg';
+import Pinclick from '@assets/pinclick.svg';
 import PublicisSapient from '@assets/publicissapient.svg';
+import Q3Tech from '@assets/q3tech.svg';
 import Samsung from '@assets/samsung.svg';
+import SamsungSDS from '@assets/samsungsds.svg';
 import ServiceNow from '@assets/servicenow.svg';
+import Snapdeal from '@assets/snapdeal.svg';
+import Tally from '@assets/tally.svg';
 import TCS from '@assets/tcs.svg';
 import Toshiba from '@assets/toshiba.svg';
+import Virtusa from '@assets/virtusa.svg';
+import Xebia from '@assets/xebia.svg';
 import Yamaha from '@assets/yamaha.svg';
 
 const companies: Record<0 | 1, Record<string, React.FC<React.SVGProps<SVGSVGElement>>>> = {
-  0: { Microsoft, Amazon, GoldmanSachs, Flipkart, Adobe, Samsung, ServiceNow },
-  1: {
+  0: {
+    Microsoft,
+    Amazon,
+    GoldmanSachs,
+    Flipkart,
+    Adobe,
+    SamsungSDS,
+    Jio,
+    Cvent,
     Capgemini,
-    PublicisSapient,
-    Yamaha,
-    Deloitte,
+    ServiceNow,
+    OYO,
     Infosys,
-    AlcatelLucent,
-    Toshiba,
-    Ericsson,
+  },
+  1: {
+    PublicisSapient,
+    Nagarro,
     TCS,
+    BrowserStack,
+    FiftyFive,
+    _314eCorp,
+    Yamaha,
+    Virtusa,
+    Delhivery,
+    Byjus,
+    Accops,
+    Icertis,
+    Tally,
+    AlcatelLucent,
+    Deloitte,
+    Xebia,
+    Snapdeal,
+    Toshiba,
+    Samsung,
+    Newgen,
+    Invenio,
+    Ericsson,
+    Q3Tech,
+    NeoSoft,
+    Fampay,
     Fanatics,
-    JIO,
     Oracle,
+    Pinclick,
+    Orange,
     CGI,
   },
 };
