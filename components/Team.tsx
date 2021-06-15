9d2a8024a88c6a3136e467c5373e31d00f9f7892
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-06-15 19:04:10 +0530

    fix: responsiveness of teams section

--- components/Team.tsx
+++ components/Team.tsx
@@ -5,25 +5,32 @@ import members from '@utils/members';
 
 const Team: React.FC = () => (
   <section cx="sect">
-    <h1 cx="title">Our Team</h1>
-    <div cx="members">
-      {members.map(({ name, desc }) => (
-        <div cx="member" key={cyrb53(name)}>
-          <Image
-            src={`/members/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
-            width="96"
-            height="96"
-            cx="image"
-            role="img"
-            alt={name}
-            aria-label={name}
-          />
-          <div cx="content">
-            <p cx="name">{name}</p>
-            <p cx="desc">{desc}</p>
+    <div cx="ctr">
+      <div cx="title-wrapper">
+        <h1 cx="title">Our Team</h1>
+      </div>
+      <div cx="members">
+        {members.map(({ name, desc }) => (
+          <div cx="member" key={cyrb53(name)}>
+            <div cx="wrapper">
+              <Image
+                src={`/members/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
+                width="96"
+                height="96"
+                cx="image"
+                role="img"
+                alt={name}
+                aria-label={name}
+              />
+
+              <div cx="content">
+                <h2 cx="name">{name}</h2>
+                <p cx="desc">{desc}</p>
+              </div>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   </section>
 );
