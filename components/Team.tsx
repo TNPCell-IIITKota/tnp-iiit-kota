61cd89288aa883923a21da7a0ccfc8541198584a
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-06-17 23:30:58 +0530

    fix: split team year-wise

--- components/Team.tsx
+++ components/Team.tsx
@@ -9,8 +9,9 @@ const Team: React.FC = () => (
       <div cx="title-wrapper">
         <h1 cx="title">Our Team</h1>
       </div>
+      <h2 cx="lead">Faculty Coordinator</h2>
       <div cx="members">
-        {members.map(({ name, desc }) => (
+        {members.faculty.map(({ name, linkedIn }) => (
           <div cx="member" key={cyrb53(name)}>
             <div cx="wrapper">
               <Image
@@ -23,9 +24,167 @@ const Team: React.FC = () => (
                 aria-label={name}
               />
 
+              <a
+                cx="overlay"
+                href={`https://www.linkedin.com/in/${linkedIn}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  fill="white"
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="0"
+                  viewBox="0 0 24 24"
+                  role="img"
+                  aria-label="linkedin"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  />
+                  <circle cx="4" cy="4" r="2" stroke="none" />
+                </svg>
+              </a>
+
               <div cx="content">
                 <h2 cx="name">{name}</h2>
-                <p cx="desc">{desc}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <h2 cx="lead">Student Placement Coordinators &mdash; Y&apos;22</h2>
+      <div cx="members">
+        {members[2022].map(({ name, linkedIn }) => (
+          <div cx="member" key={cyrb53(name)}>
+            <div cx="wrapper">
+              <Image
+                src={`/members/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
+                width="96"
+                height="96"
+                cx="image"
+                role="img"
+                alt={name}
+                aria-label={name}
+              />
+
+              <a
+                cx="overlay"
+                href={`https://www.linkedin.com/in/${linkedIn}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  fill="white"
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="0"
+                  viewBox="0 0 24 24"
+                  role="img"
+                  aria-label="linkedin"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  />
+                  <circle cx="4" cy="4" r="2" stroke="none" />
+                </svg>
+              </a>
+
+              <div cx="content">
+                <h2 cx="name">{name}</h2>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <h2 cx="lead">Former Student Placement Coordinators</h2>
+      <div cx="members">
+        {members[2020].map(({ name, linkedIn }) => (
+          <div cx="member" key={cyrb53(name)}>
+            <div cx="wrapper">
+              <Image
+                src={`/members/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
+                width="96"
+                height="96"
+                cx="image"
+                role="img"
+                alt={name}
+                aria-label={name}
+              />
+
+              <a
+                cx="overlay"
+                href={`https://www.linkedin.com/in/${linkedIn}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  fill="white"
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="0"
+                  viewBox="0 0 24 24"
+                  role="img"
+                  aria-label="linkedin"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  />
+                  <circle cx="4" cy="4" r="2" stroke="none" />
+                </svg>
+              </a>
+
+              <div cx="content">
+                <h2 cx="name">{name}</h2>
+              </div>
+            </div>
+          </div>
+        ))}
+        {members[2021].map(({ name, linkedIn }) => (
+          <div cx="member" key={cyrb53(name)}>
+            <div cx="wrapper">
+              <Image
+                src={`/members/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
+                width="96"
+                height="96"
+                cx="image"
+                role="img"
+                alt={name}
+                aria-label={name}
+              />
+
+              <a
+                cx="overlay"
+                href={`https://www.linkedin.com/in/${linkedIn}`}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <svg
+                  fill="white"
+                  stroke="white"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="0"
+                  viewBox="0 0 24 24"
+                  role="img"
+                  aria-label="linkedin"
+                >
+                  <path
+                    stroke="none"
+                    d="M16 8a6 6 0 016 6v7h-4v-7a2 2 0 00-2-2 2 2 0 00-2 2v7h-4v-7a6 6 0 016-6zM2 9h4v12H2z"
+                  />
+                  <circle cx="4" cy="4" r="2" stroke="none" />
+                </svg>
+              </a>
+
+              <div cx="content">
+                <h2 cx="name">{name}</h2>
               </div>
             </div>
           </div>
--- utils/members.ts
+++ utils/members.ts
@@ -1,15 +1,24 @@
-const members = [
-  { name: 'Dr. Ashish Sharma', desc: 'Faculty Coordinator' },
-  { name: 'Mohit Choudhary', desc: "ECE '20" },
-  { name: 'Shrikant Deshmane', desc: "CSE '20" },
-  { name: 'Sameer Sinha', desc: "CSE '21" },
-  { name: 'Shashwat Singh', desc: "CSE '21" },
-  { name: 'Anushka Joshi', desc: "ECE '22" },
-  { name: 'Inderjeet', desc: "CSE '22" },
-  { name: 'Mayank Goyal', desc: "CSE '22" },
-  { name: 'Rajkumar Pareek', desc: "ECE '22" },
-  { name: 'Sanatan Shrivastava', desc: "CSE '22" },
-  { name: 'Divyansh Singh', desc: "CSE '23" },
-];
+const members: Record<
+  'faculty' | 2020 | 2021 | 2022 | 2023,
+  Array<{ name: string; linkedIn: string }>
+> = {
+  faculty: [{ name: 'Dr. Ashish Sharma', linkedIn: 'dr-ashish-sharma-a6a5491b' }],
+  2020: [
+    { name: 'Mohit Choudhary', linkedIn: 'mohit-choudhary-30715a159' },
+    { name: 'Shrikant Deshmane', linkedIn: 'shrikant-deshmane-439791147' },
+  ],
+  2021: [
+    { name: 'Sameer Sinha', linkedIn: 'sameer-sinha-a1a7a41b1' },
+    { name: 'Shashwat Singh', linkedIn: 's10singh97' },
+  ],
+  2022: [
+    { name: 'Anushka Joshi', linkedIn: '' },
+    { name: 'Inderjeet', linkedIn: 'inderjeet-816b88176' },
+    { name: 'Mayank Goyal', linkedIn: 'mayankkgoyal' },
+    { name: 'Rajkumar Pareek', linkedIn: 'rajkumar-pareek-06a573170' },
+    { name: 'Sanatan Shrivastava', linkedIn: 'sanatan-shrivastava-04a327171' },
+  ],
+  2023: [{ name: 'Divyansh Singh', linkedIn: 'brc-dd' }],
+};
 
 export default members;
