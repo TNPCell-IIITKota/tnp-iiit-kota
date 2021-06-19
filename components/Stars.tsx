78cf4c4f9bf7961d7ebf91d05225d1a74cb78d72
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-06-20 01:41:13 +0530

    feat: add previous years' star performers

--- components/Stars.tsx
+++ components/Stars.tsx
@@ -3,6 +3,39 @@ import Image from 'next/image';
 import cyrb53 from '@utils/hash-string';
 import topPerformers from '@utils/star-perf';
 
+const Wrapper: React.FC<{ year: keyof typeof topPerformers }> = ({ year }) => (
+  <>
+    {topPerformers[year].map(({ name, company, lpa }) => (
+      <div cx="star" key={cyrb53(name)}>
+        <div cx="wrapper">
+          <div cx="image-wrapper">
+            <Image
+              src={`/stars/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
+              width="160"
+              height="160"
+              cx="image"
+              role="img"
+              alt={name}
+              aria-label={name}
+            />
+          </div>
+
+          <div cx="overlay-wrapper">
+            <h2 cx="name">
+              {name.substr(0, name.indexOf(' '))}
+              <br />
+              {name.substr(name.indexOf(' ') + 1)}
+              <span cx="year">&nbsp;Y&apos;{year}</span>
+            </h2>
+            <h3 cx="company">{company}</h3>
+            <p cx="lpa">{lpa} LPA</p>
+          </div>
+        </div>
+      </div>
+    ))}
+  </>
+);
+
 const Stars: React.FC = () => (
   <section cx="sect" id="featured-stars">
     <div cx="ctr">
@@ -11,31 +44,15 @@ const Stars: React.FC = () => (
         <p cx="desc">Meet some of our alumni who secured the highest packages.</p>
       </div>
       <div cx="stars">
-        {topPerformers.map(({ name, company, lpa }) => (
-          <div cx="star" key={cyrb53(name)}>
-            <div cx="wrapper">
-              <div cx="image-wrapper">
-                <Image
-                  src={`/stars/${name.toLowerCase().replace(/\.?\s+/g, '-')}.jpg`}
-                  width="160"
-                  height="160"
-                  cx="image"
-                  role="img"
-                  alt={name}
-                  aria-label={name}
-                />
-              </div>
-
-              <div cx="overlay-wrapper">
-                <h2 cx="name">{name}</h2>
-                <h3 cx="company">{company}</h3>
-                <p cx="lpa">{lpa} LPA</p>
-              </div>
-            </div>
-          </div>
-        ))}
+        {Object.keys(topPerformers)
+          .sort()
+          .reverse()
+          .map((year) => (
+            <Wrapper year={year as keyof typeof topPerformers} key={cyrb53(`stars-y'${year}`)} />
+          ))}
       </div>
     </div>
   </section>
 );
+
 export default Stars;
--- utils/star-perf.ts
+++ utils/star-perf.ts
@@ -1,9 +1,38 @@
-const topPerformers = [
-  { name: 'Jayansh Goyal', company: 'Microsoft', lpa: 43 },
-  { name: 'Shivam Garg', company: 'Amazon', lpa: 32 },
-  { name: 'Vibhor Rawal', company: 'Gameskraft', lpa: 32 },
-  { name: 'Sudeep Singh Gill', company: 'Flipkart', lpa: 26.5 },
-  { name: 'Ashish Uniyal', company: 'Goldman Sachs', lpa: 23 },
-];
+const topPerformers: Record<
+  '17' | '18' | '19' | '20' | '21',
+  Array<{ name: string; company: string; lpa: number }>
+> = {
+  21: [
+    { name: 'Jayansh Goyal', company: 'Microsoft', lpa: 43 },
+    { name: 'Shivam Garg', company: 'Amazon', lpa: 32 },
+    { name: 'Vibhor Rawal', company: 'Gameskraft', lpa: 32 },
+    { name: 'Sudeep Singh Gill', company: 'Flipkart', lpa: 26.5 },
+    { name: 'Ashish Uniyal', company: 'Goldman Sachs', lpa: 23 },
+  ],
+
+  20: [
+    { name: 'Shubham Guglani', company: 'Amazon', lpa: 29 },
+    { name: 'Pratik Pandey', company: 'ServiceNow', lpa: 25 },
+    { name: 'Tejeshwar Saini', company: 'Flipkart', lpa: 24 },
+    { name: 'Akshra Gupta', company: 'Goldman Sachs', lpa: 23 },
+    { name: 'Shrishti Gupta', company: 'Goldman Sachs', lpa: 23 },
+    { name: 'Vipasha Chandwani', company: 'Goldman Sachs', lpa: 23 },
+  ],
+
+  19: [
+    { name: 'Nilesh Agarwal', company: 'Amazon', lpa: 27 },
+    { name: 'Nikhil Arya', company: 'ServiceNow', lpa: 21 },
+  ],
+
+  18: [
+    { name: 'Gaurav Agarwal', company: 'Amazon', lpa: 27 },
+    { name: 'Nikhil Chetule', company: 'Amazon', lpa: 27 },
+    // TODO: add image
+    // { name: 'Ravi Malpani', company: 'Amazon', lpa: 27 },
+    { name: 'Yash Sharma', company: 'Amazon', lpa: 27 },
+  ],
+
+  17: [{ name: 'Arushi Gupta', company: 'Amazon', lpa: 26 }],
+};
 
 export default topPerformers;
