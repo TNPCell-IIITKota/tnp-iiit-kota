43a9fc220b932df7d619beeeea6f13cf17f4457f
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-06-22 15:21:08 +0530

    fix: hover glitches on slider component

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,3 @@
-// const defaultExtractor = (content) => {
-//   const matches = content.match(/"(.+?)"/g) || [];
-//   const set = new Set(matches.map((s) => s.slice(1, -1).split(' ')).flat());
-//   set.delete('');
-//   return [...set];
-// };
-
 module.exports = {
   plugins: {
     tailwindcss: {},
@@ -14,10 +7,5 @@ module.exports = {
       features: { 'custom-properties': false },
       stage: 3,
     },
-    // '@fullhuman/postcss-purgecss': {
-    //   content: ['./pages/**/*.tsx', './components/**/*.tsx'],
-    //   defaultExtractor,
-    //   safelist: [...],
-    // },
   },
 };
