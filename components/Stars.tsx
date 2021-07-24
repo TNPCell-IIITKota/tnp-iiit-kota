b6d5d564fbeb68de2e567a9f7e7912ba95d91efb
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-07-24 21:40:52 +0530

    feat: add skeleton for faqs

--- components/Stars.tsx
+++ components/Stars.tsx
@@ -35,7 +35,7 @@ const Stars: React.FC = () => (
       <div cx="title-wrapper">
         <h1 cx="title">Featured Stars</h1>
         <p cx="desc">
-          The alumni of IIIT Kota have made their mark on the various government, academia &amp;
+          The alumni of IIIT Kota have made their mark on various government, academia &amp;
           corporate fronts. Meet some of our alumni who we are proud of producing.
         </p>
       </div>
