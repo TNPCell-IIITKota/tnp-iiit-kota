79f79d456d271239c7bac640144295fd83d3c63d
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-07-25 14:30:18 +0530

    fix: separate styles of faqs section

--- components/FAQs.tsx
+++ components/FAQs.tsx
@@ -1,17 +1,14 @@
 const Question: React.FC = ({ children }) => (
-  <div className="p-2 w-full lg:w-1/2">
-    <button
-      type="button"
-      className="flex relative items-center p-4 w-full h-full bg-gray-100 rounded"
-    >
-      <span className="font-semibold">{children}</span>
+  <div cx="wrapper">
+    <button type="button" cx="btn">
+      <span cx="question">{children}</span>
       <svg
         fill="none"
         stroke="currentColor"
         strokeLinecap="round"
         strokeLinejoin="round"
         strokeWidth="3"
-        className="absolute right-0 flex-shrink-0 mr-4 w-6 h-6 text-indigo-500"
+        cx="icon"
         viewBox="0 0 24 24"
       >
         <path
@@ -26,13 +23,11 @@ const Question: React.FC = ({ children }) => (
 );
 
 const FAQs: React.FC = () => (
-  <section className="mx-auto mt-4 text-gray-600 xl:w-3/5">
-    <div className="container px-5 pt-16 mx-auto border-t border-gray-200">
-      <h1 className="mt-2 mb-14 text-2xl font-semibold text-center text-gray-900 sm:text-3xl">
-        Frequently Asked Questions
-      </h1>
+  <section cx="sect">
+    <div cx="ctr">
+      <h1 cx="title">Frequently Asked Questions</h1>
 
-      <div className="flex flex-wrap -mx-2 sm:mx-auto sm:mb-2">
+      <div cx="questions">
         <Question>Authentic Cliche Forage</Question>
         <Question>Kinfolk Chips Snackwave</Question>
         <Question>Coloring Book Ethical</Question>
@@ -41,7 +36,7 @@ const FAQs: React.FC = () => (
         <Question>The Catcher In The Rye</Question>
       </div>
 
-      <p className="mx-auto mt-4 text-sm leading-relaxed text-center">
+      <p cx="desc">
         Can&apos;t find the answers you&apos;re looking for? Reach out to our team using the
         information given below.
       </p>
