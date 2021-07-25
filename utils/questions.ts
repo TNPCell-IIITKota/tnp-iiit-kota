bc5fcefd9280e018ccba31c9c0fa08037c46a168
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-07-25 16:20:40 +0530

    fix: organise questions

---
+++ utils/questions.ts
@@ -0,0 +1,28 @@
+const questions: Array<{ question: string; answer: string }> = [
+  {
+    question: 'How much damage could your best friend do to you if you ever had a falling out?',
+    answer: 'Answer 1',
+  },
+  {
+    question: "What's the most amount of money you've ever found? What was the situation?",
+    answer: 'Answer 2',
+  },
+  {
+    question: "What's a situation when you look back you should have gone ahead and done it?",
+    answer: 'Answer 3',
+  },
+  {
+    question: 'What were you most afraid of when you were younger? Do you still have that fear?',
+    answer: 'Answer 4',
+  },
+  {
+    question: "What's something that you recently learned that everyone else already knew?",
+    answer: 'Answer 5',
+  },
+  {
+    question: "What do you consider to be the smartest thing you've ever done in your life so far?",
+    answer: 'Answer 6',
+  },
+];
+
+export default questions;
