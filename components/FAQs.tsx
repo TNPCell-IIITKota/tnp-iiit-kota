9d59d953bfe2a9b3b9a41bb470ffef0473fa3259
Divyansh Singh <40380293+brc-dd@users.noreply.github.com>
2021-07-26 16:35:14 +0530

    feat: add modal for answers

--- components/FAQs.tsx
+++ components/FAQs.tsx
@@ -1,26 +1,42 @@
+import { useRef } from 'react';
+
+import Modal from '@components/Modal';
 import Question from '@components/Question';
 import cyrb53 from '@utils/hash-string';
 import questions from '@utils/questions';
 
-const FAQs: React.FC = () => (
-  <section cx="sect">
-    <div cx="ctr">
-      <h1 cx="title">Frequently Asked Questions</h1>
+const FAQs: React.FC = () => {
+  const modalRef = useRef<React.ElementRef<typeof Modal>>(null);
 
-      <div cx="questions">
-        {questions.map(({ question: q, answer: a }) => (
-          <Question key={cyrb53(q)} answer={a}>
-            {q}
-          </Question>
-        ))}
-      </div>
+  return (
+    <>
+      <section cx="sect">
+        <div cx="ctr">
+          <h1 cx="title">Frequently Asked Questions</h1>
 
-      <p cx="desc">
-        Can&apos;t find the answers you&apos;re looking for? Reach out to our team using the
-        information given below.
-      </p>
-    </div>
-  </section>
-);
+          <div cx="questions">
+            {questions.map(({ question: q, answer: a }) => (
+              <Question
+                key={cyrb53(q)}
+                onClick={(): void => {
+                  modalRef.current!.setAnswer(a);
+                  modalRef.current!.setOpen(true);
+                }}
+              >
+                {q}
+              </Question>
+            ))}
+          </div>
+
+          <p cx="desc">
+            Can&apos;t find the answers you&apos;re looking for? Reach out to our team using the
+            information given below.
+          </p>
+        </div>
+      </section>
+      <Modal ref={modalRef} />
+    </>
+  );
+};
 
 export default FAQs;
--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,21 +1,40 @@
-import { Fragment, useRef, useState } from 'react';
+import React, {
+  forwardRef,
+  Fragment,
+  useEffect,
+  useImperativeHandle,
+  useRef,
+  useState,
+} from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 
-const Modal: React.FC = () => {
-  const [open, setOpen] = useState(true);
-  const cancelButtonRef = useRef(null);
+const Modal: React.ForwardRefRenderFunction<{
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  setAnswer: React.Dispatch<React.SetStateAction<string>>;
+}> = (_, forwardedRef) => {
+  const [open, setOpen] = useState(false);
+  const [answer, setAnswer] = useState('');
+  const btnRef = useRef<HTMLButtonElement>(null);
+
+  useImperativeHandle(forwardedRef, () => ({ setOpen, setAnswer }));
+
+  useEffect(() => {
+    const observer = new MutationObserver((): void => {
+      document.querySelector('header')!.style.width = `calc(100% - ${
+        document.documentElement.style.paddingRight || '0px'
+      })`;
+    });
+
+    observer.observe(document.documentElement, { attributes: true, attributeFilter: ['style'] });
+    return (): void => {
+      observer.disconnect();
+    };
+  }, []);
 
   return (
-    <Transition.Root show={open} as={Fragment}>
-      <Dialog
-        as="div"
-        static
-        className="overflow-y-auto fixed inset-0 z-10"
-        initialFocus={cancelButtonRef}
-        open={open}
-        onClose={setOpen}
-      >
-        <div className="flex justify-center items-end px-4 pt-4 pb-20 min-h-screen text-center sm:block sm:p-0">
+    <Transition appear show={open} as={Fragment}>
+      <Dialog as="div" cx="modal" onClose={setOpen} initialFocus={btnRef}>
+        <div cx="ctr">
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -25,10 +44,10 @@ const Modal: React.FC = () => {
             leaveFrom="opacity-100"
             leaveTo="opacity-0"
           >
-            <Dialog.Overlay className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+            <Dialog.Overlay cx="overlay" />
           </Transition.Child>
 
-          <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">
+          <span cx="fake" aria-hidden="true">
             &#8203;
           </span>
           <Transition.Child
@@ -40,49 +59,27 @@ const Modal: React.FC = () => {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="inline-block overflow-hidden text-left align-bottom bg-white rounded-lg shadow-xl transition-all transform sm:my-8 sm:align-middle sm:max-w-lg sm:w-full">
-              <div className="px-4 pt-5 pb-4 bg-white sm:p-6 sm:pb-4">
-                <div className="sm:flex sm:items-start">
-                  <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                    <Dialog.Title as="h3" className="text-lg font-medium leading-6 text-gray-900">
-                      Deactivate account
-                    </Dialog.Title>
-                    <div className="mt-2">
-                      <p className="text-sm text-gray-500">
-                        Are you sure you want to deactivate your account? All of your data will be
-                        permanently removed. This action cannot be undone.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="py-3 px-4 bg-gray-50 sm:px-6 sm:flex sm:flex-row-reverse">
+            <div cx="wrapper">
+              <div cx="ans">{answer}</div>
+
+              <div cx="btn-wrapper">
                 <button
                   type="button"
-                  className="inline-flex justify-center py-2 px-4 w-full text-base font-medium text-white bg-red-600 rounded-md border border-transparent shadow-sm hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
+                  cx="btn"
                   onClick={(): void => {
                     setOpen(false);
                   }}
+                  ref={btnRef}
                 >
-                  Deactivate
-                </button>
-                <button
-                  type="button"
-                  className="inline-flex justify-center py-2 px-4 mt-3 w-full text-base font-medium text-gray-700 bg-white rounded-md border border-gray-300 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={(): void => {
-                    setOpen(false);
-                  }}
-                  ref={cancelButtonRef}
-                >
-                  Cancel
+                  OK
                 </button>
               </div>
             </div>
           </Transition.Child>
         </div>
       </Dialog>
-    </Transition.Root>
+    </Transition>
   );
 };
 
-export default Modal;
+export default forwardRef(Modal);
