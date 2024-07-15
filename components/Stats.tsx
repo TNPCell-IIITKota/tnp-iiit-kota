bcd9f872636776b81d0d845c948febc0015e8b1a
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-07-15 22:08:04 +0530

    feat(2024): updating stats

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -30,7 +30,7 @@ const Stats: React.FC = () => (
               />
             </svg>
           </a>
-          <p cx="additional-info">*These stats are last updated as per June 30th, 2024. </p>
+          <p cx="additional-info">*These stats are last updated as per July 15th, 2024. </p>
           <span style={{ fontSize: '14px' }}>
             Subject to get updated with final placement report{' '}
           </span>
