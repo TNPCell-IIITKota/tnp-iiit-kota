9f8b6b46f4d35d294214ec08d1a93e360a26aa5a
Dhairya Majmudar <2022kuec2045@iiitkota.ac.in>
2024-07-24 21:05:01 +0530

    feat(2024): add achivers

--- utils/achivers.ts
+++ utils/achivers.ts
@@ -7,34 +7,18 @@ const exams = [
 
 const achivers: Record<'24', Array<{ name: string; achievements: Array<string> }>> = {
   24: [
-    // { name: 'Ashish Soni', achievements: [`C4GT'24`, `GSoC'23 Tor`] },
-    // { name: 'Sankalp Pareekh', achievements: [`GSoC'24 GFOSS`] },
-    // {
-    //   name: 'Dhairya Majmudar',
-    //   achievements: [`GSoD'24 JSON Schema`, `SoK'24 KDE`],
-    // ,
-    // },
-    // { name: 'Tarsh Sawarkar', achievements: [`Amazon ML Summer Camp.`] },
-    // { name: 'Aaryan Singh', achievements: [`Amazon ML Summer Camp.`] },
-    // { name: 'Srishti Singh', achievements: [`Google WE Scholar'24`] },
-    // { name: 'Aniska Verma', achievements: [`Google WE Scholar'24`] },
-    // {
-    //   name: 'Pranam Srivastava',
-    //   achievements: [`Infosys SpringBoard'24`],
-    // ,
-    // },
-    { name: 'Ankur Agarwal', achievements: [`C4GT'24`, `GSoC'23 Tor`] },
-    { name: 'Ankur Agarwal', achievements: [`GSoC'24 GFOSS`] },
+    { name: 'Sankalp Pareek', achievements: [`GSoC'24 - GFOSS`] },
+    { name: 'Ashish Soni', achievements: [`C4GT'24`, `GSoC'23 - Tor`] },
     {
-      name: 'Ankur Agarwal',
+      name: 'Dhairya Majmudar',
       achievements: [`GSoD'24 - JSON Schema`, `SoK'24 - KDE`],
     },
-    { name: 'Ankur Agarwal', achievements: [`Amazon ML Summer Camp.`] },
-    { name: 'Ankur Agarwal', achievements: [`Amazon ML Summer Camp.`] },
-    { name: 'Ankur Agarwal', achievements: [`Amazon ML Summer Camp.`] },
-    { name: 'Ankur Agarwal', achievements: [`Google WE Scholar'24`] },
-    { name: 'Ankur Agarwal', achievements: [`Google WE Scholar'24`] },
-    { name: 'Ankur Agarwal', achievements: [`Infosys SpringBoard'24`] },
+    { name: 'Aaryan Singh', achievements: [`Amazon ML Summer School'24`] },
+    { name: 'Tarsh Swarnkar', achievements: [`Amazon ML Summer School'24`] },
+    { name: 'Lakshita Saxena', achievements: [`Amazon ML Summer School'24`] },
+    { name: 'Srishti Singh', achievements: [`Google WE Scholar'24`] },
+    { name: 'Anishka Verma', achievements: [`Google WE Scholar'24`] },
+    { name: 'Pranam Srivastava', achievements: [`Infosys SpringBoard'24`] },
   ],
 };
 
